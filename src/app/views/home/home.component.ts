bef5fb8ab7e30ed6147567c3ef4e855ce2051e62
Milos Brkljacic <milosbr@gmail.com>
2024-11-08 21:57:37 +0100

    add example select to home component

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,12 +1,17 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { TableExampleComponent } from '../../shared/components/table-example/table-example.component';
+import { MatButtonToggle, MatButtonToggleGroup } from '@angular/material/button-toggle';
+
+import { FlexExampleComponent } from '@sharedComponents/flex-example/flex-example.component';
+import { TableExampleComponent } from '@sharedComponents/table-example/table-example.component';
 
 @Component({
   selector: 'pltr-home',
   standalone: true,
-  imports: [TableExampleComponent],
+  imports: [TableExampleComponent, FlexExampleComponent, MatButtonToggle, MatButtonToggleGroup],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent {}
+export class HomeComponent {
+  exampleSelection: 'table' | 'flex' = 'table';
+}
