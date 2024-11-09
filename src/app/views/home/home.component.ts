c8719a6f4b042dec4b006fe12e8400a5fcdedf0c
Milos Brkljacic <milosbr@gmail.com>
2024-11-09 23:03:19 +0100

    add method for each action in home component

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { MatButtonToggle, MatButtonToggleGroup } from '@angular/material/button-toggle';
+import { MatButton } from '@angular/material/button';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 
@@ -7,13 +8,13 @@ import { FlexExampleComponent } from '@sharedComponents/flex-example/flex-exampl
 import { TableExampleComponent } from '@sharedComponents/table-example/table-example.component';
 import { Chapter } from '@models/chapter';
 import { Plotline } from '@models/plotline';
-import { AppActions } from '@store/actions/app.actions';
+import { AppActions, ChaptersActions, PlotlinesActions } from '@store/actions/app.actions';
 import { selectAllChapters, selectAllPlotlines } from '@store/reducers/app.reducers';
 
 @Component({
   selector: 'pltr-home',
   standalone: true,
-  imports: [TableExampleComponent, FlexExampleComponent, MatButtonToggle, MatButtonToggleGroup],
+  imports: [TableExampleComponent, FlexExampleComponent, MatButton, MatButtonToggle, MatButtonToggleGroup],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -29,5 +30,33 @@ export class HomeComponent implements OnInit {
     this.plotlinesData$ = this.store.select(selectAllPlotlines);
     this.chaptersData$ = this.store.select(selectAllChapters);
     this.store.dispatch(AppActions.loadData());
+  }
+
+  addChapter(): void {
+    this.store.dispatch(ChaptersActions.addChapter());
+  }
+
+  moveChapter(chapterId: string, oldIndex: number, newIndex: number): void {
+    this.store.dispatch(ChaptersActions.moveChapter({ chapterId, oldIndex, newIndex }));
+  }
+
+  updateChapterName(chapterId: string, chapterName: string): void {
+    this.store.dispatch(ChaptersActions.updateChapterName({ chapterId, chapterName }));
+  }
+
+  addPlotline(): void {
+    this.store.dispatch(PlotlinesActions.addPlotline());
+  }
+
+  updatePlotlineName(plotlineId: string, plotlineName: string): void {
+    this.store.dispatch(PlotlinesActions.updatePlotlineName({ plotlineId, plotlineName }));
+  }
+
+  addScene(plotlineId: string, chapterId: string, index: number): void {
+    this.store.dispatch(PlotlinesActions.addScene({ plotlineId, chapterId, index }));
+  }
+
+  updateSceneDescription(plotlineId: string, chapterId: string, sceneId: string, description: string): void {
+    this.store.dispatch(PlotlinesActions.updateSceneDescription({ plotlineId, chapterId, sceneId, description }));
   }
 }
